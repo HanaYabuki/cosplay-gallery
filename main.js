1e44dd61db71e844a962ea5075fbe8bd7b9f26e2
HanaYabuki <1847196775@qq.com>
2023-12-06 00:46:35 +0800

    支持缩略图 加速访问

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const jsonRoot = 'info.json'
+const jsonRoot = 'https://cdn.jsdelivr.net/gh/HanaYabuki/cosplay-db@master/main.json'
 const urlRoot = '//cdn.jsdelivr.net/gh/HanaYabuki/cosplay-db@master/'
 
 async function view() {
@@ -8,7 +8,10 @@ async function view() {
             return d.map(i => ({
                 ...i,
                 link: '/gallery.html?g=' + encodeURIComponent(i.title),
-                photosSrc: i.photos.map(p => urlRoot + i.root + '/' + p)
+                photosSrc: i.photos.map(p => ({
+                    fast: urlRoot + i.root.replace('img', 'fastimg') + '/' + p,
+                    raw: urlRoot + i.root + '/' + p
+                }))
             }))
         })
         .catch(e => console.log(e))
